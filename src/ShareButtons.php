9dee997a41be8b4fc576e2d8bc7a37604c9a2568
Евгений <89125230101@mail.ru>
2016-02-20 12:57:45 +0500

    relase

--- src/ShareButtons.php
+++ src/ShareButtons.php
@@ -5,14 +5,24 @@ use yii\base\Widget;
 class ShareButtons extends Widget{
 
     public $clientOptions=[];
+    private $options="";
 
     public function init(){
         parent::init();
+        foreach ($this->clientOptions as $key=>$option){
+            if (is_bool($option)){
+                $this->options.="data-".$key." ";
+            }
+            else {
+                $this->options.="data-".$key.'="'.$option.'" ';
+            }
+
+        }
+        $this->options=trim($this->options);
     }
 
     public function run(){
-        var_dump($this->clientOptions);
-        return $this->render('widget');
+        return $this->render('widget',['options'=>$this->options]);
     }
 
 }
--- src/views/widget.php
+++ src/views/widget.php
@@ -1,1 +1,9 @@
-<h1>Социальные кнопки</h1>
+<?
+    $this->registerJsFile('https://yastatic.net/es5-shims/0.0.2/es5-shims.min.js', ['async' => 'async']);
+    $this->registerJsFile('https://yastatic.net/share2/share.js', ['async' => 'async']);
+?>
+
+<div class="ya-share2" <?=$options?>></div>
+
+<?$this->registerJs("Ya.share2('.ya-share2')");?>
+
