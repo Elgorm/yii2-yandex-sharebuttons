d545f5af1463f2b970152fc6fcc2ca0dd05b8a57
mitrii <p.mitrii@gmail.com>
2016-04-12 12:11:06 +0600

    fix shorttags

--- src/views/widget.php
+++ src/views/widget.php
@@ -1,10 +1,11 @@
-<?
-    use yii\helpers\Html;
-    $this->registerJsFile('https://yastatic.net/es5-shims/0.0.2/es5-shims.min.js');
-    $this->registerJsFile('https://yastatic.net/share2/share.js');
+<?php
+    
+use yii\helpers\Html;
+    
+$this->registerJsFile('https://yastatic.net/es5-shims/0.0.2/es5-shims.min.js');
+$this->registerJsFile('https://yastatic.net/share2/share.js');
 ?>
-<?
-    echo Html::tag($tag,"",$options);
-?>
-<?$this->registerJs("Ya.share2('#".$options['id']."')");?>
+
+<?= Html::tag($tag,"",$options); ?>
+<?php $this->registerJs("Ya.share2('#".$options['id']."')");?>
 
