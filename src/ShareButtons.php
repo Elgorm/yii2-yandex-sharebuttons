ace5e517716d07e3acbb5f8101eb8da748ed41d4
Evgeny <web@andrey-company.ru>
2021-09-23 13:35:41 +0500

    fixed

--- src/ShareButtons.php
+++ src/ShareButtons.php
@@ -11,23 +11,24 @@ class ShareButtons extends Widget{
         'data-copy' => 'last',
         'data-counter' => false,
         'data-curtain' => true,
-        'data-description' => '',
+        'data-description' => false,
         'data-direction' => 'horizontal',
-        'data-hashtags' => '',
-        'data-image' => '',
+        'data-hashtags' => false,
+        'data-image' => false,
         'data-lang' => 'ru',
         'data-limit' => false,
         'data-more-button-type' => false,
-        'data-nonce' => '',
+        'data-nonce' => false,
         'data-popup-direction' => 'bottom',
         'data-popup-position' => 'inner',
         'data-services' => 'collections,vkontakte,facebook,twitter',
         'data-shape' => 'normal',
         'data-size' => 'm',
-        'data-title' => '',
-        'data-url' => '',
+        'data-title' => false,
+        'data-url' => false,
         'data-use-links' => false
     );
+    private $tag = 'div';
 
     public function init(){
         parent::init();
@@ -38,12 +39,14 @@ class ShareButtons extends Widget{
             $this->clientOptions['id'] = $this->getId();
         }
 
-        if (!isset($this->clientOptions['tag'])) {
-            $this->clientOptions['tag'] = 'div';
-        } else {
-            $this->clientOptions['tag'] = $this->clientOptions['tag'];
+        if (isset($this->clientOptions['tag'])) {
+            $this->tag = $this->clientOptions['tag'];
+            unlink($this->clientOptions['tag']);
         }
 
+
+
+        $options = $this->options;
 
 
         foreach ($this->clientOptions as $key => $option){
@@ -62,11 +65,12 @@ class ShareButtons extends Widget{
                 $options[$key] = $option;
             }
         }
-        $this->options = array_merge($options, $this->clientOptions);
+
+        $this->options = $options;
     }
 
     public function run(){
-        return $this->render('widget',['options' => $this->options,'tag' => $this->clientOptions['tag']]);
+        return $this->render('widget',['options' => $this->options,'tag' => $this->tag]);
     }
 
 }
